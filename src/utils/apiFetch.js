2f974aa9a393cc308c4a1ed4a5495b9181e2ab08
Benoit-Monteiro-Da-Silva <benoit.monteirods@gmail.com>
2025-06-13 11:44:41 +0200

    chore(env): add .env.example for API KEY

--- src/utils/apiFetch.js
+++ src/utils/apiFetch.js
@@ -2,7 +2,7 @@ import { API } from "../constants/api"
 
 export const findPopularTvShows = async () => {
     try {
-        const apiData = await fetch(`${API.BASE_URL}/3/tv/popular?api_key=${API.KEY}`)
+        const apiData = await fetch(`${API.BASE_URL}/3/tv/popular?api_key=${import.meta.env.VITE_API_KEY_PARAM}`)
         const response = await apiData.json()
         return response
     } catch(error) {
@@ -13,7 +13,7 @@ export const findPopularTvShows = async () => {
 
 export const findRecommandations = async (id) => {
     try {
-        const apiData = await fetch(`${API.BASE_URL}/3/tv/${id}/recommendations?api_key=${API.KEY}`)
+        const apiData = await fetch(`${API.BASE_URL}/3/tv/${id}/recommendations?api_key=${import.meta.env.VITE_API_KEY_PARAM}`)
         const response = await apiData.json()
         return response
     } catch(error) {
@@ -24,7 +24,7 @@ export const findRecommandations = async (id) => {
 
 export const findTvShow = async (id) => {
     try {
-        const apiData = await fetch(`https://api.themoviedb.org/3/tv/${id}?api_key=${API.KEY}&append_to_response=credits`)
+        const apiData = await fetch(`https://api.themoviedb.org/3/tv/${id}?api_key=${import.meta.env.VITE_API_KEY_PARAM}&append_to_response=credits`)
         const response = await apiData.json()
         return response
     } catch(error) {
